8376d2e582ea892cdc92bd54395674f9cb318970
RomanArturovich <romankurenok.cens@gmail.com>
2024-09-04 15:05:46 +0300

    changed the description of the relationships between tables in db

--- HrSystem/Data/HrContext.cs
+++ HrSystem/Data/HrContext.cs
@@ -5,13 +5,26 @@ namespace HrSystem.Data
 {
     public class HrContext: DbContext
     {
+        public HrContext(DbContextOptions<HrContext> options) : base(options)
+        {
+        }
+
         public DbSet<Department> Departments { get; set; }
         public DbSet<Vacancy> Vacancies { get; set; }
         public DbSet<Candidate> Candidates { get; set; }
         public DbSet<Hr> Hrs { get; set; }
 
-        public HrContext(DbContextOptions<HrContext> options) : base(options)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Candidate>()
+                .HasOne(c => c.Vacancy)
+                .WithMany(v => v.Candidates)
+                .HasForeignKey(c => c.VacancyId);
+
+            modelBuilder.Entity<Vacancy>()
+                .HasOne(v => v.Department)
+                .WithMany(d => d.Vacancies)
+                .HasForeignKey(v => v.DepartmentId);
         }
     }
 }
